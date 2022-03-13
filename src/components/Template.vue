6410d78b5e9551d3982b1b29294b750df0fa0960
arif <muhdarifrawi@gmail.com>
2022-03-13 17:46:06 +0800

    initial commit.

---
+++ src/components/Template.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>{{test}}</p>
+</template>
+<script>
+export default {
+    name:"Template",
+    data(){
+        return{
+            test:"test"
+        }
+    }
+}
+</script>
+<style>
+
+</style>
